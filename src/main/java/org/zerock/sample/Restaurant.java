e7835b590118265db10aef4b62a37ea4b2e62283
pedroyoung95 <jty9531@gmail.com>
2021-01-06 15:13:01 +0900

    ex00 first push

---
+++ src/main/java/org/zerock/sample/Restaurant.java
@@ -0,0 +1,14 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Component
+@Data
+public class Restaurant {
+
+	@Setter
+	private Chef chef;
+}
